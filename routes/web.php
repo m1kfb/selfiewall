f1d053e6cf0c87212467e472da1eaca6a2e2e8b3
Mike Brown <mike@ordermarket.com>
2021-06-12 09:56:14 +0100

    Auth test

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ItemController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -27,3 +28,6 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->name('dashboard');
+Route::group(['auth:sanctum', 'verified'], function() {
+    Route::resource('item', ItemController::class);
+});
